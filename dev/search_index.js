5d58c1acc0c440cffd2eceb39340d5c336dd65ca
Documenter.jl <documenter@juliadocs.github.io>
2021-02-26 12:33:49 +0000

    build based on 4544cb6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlateReaderCore","category":"page"},{"location":"#PlateReaderCore","page":"Home","title":"PlateReaderCore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlateReaderCore]","category":"page"}]
+}
